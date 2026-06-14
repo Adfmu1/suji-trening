a151da690b5c6912c5f2c38fa20232fb985891af
Adfmu1 <Adfmu3@gmail.com>
2026-06-14 18:47:51 +0200

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,19 @@
+package sujitrening
+
+import (
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	mux := http.NewServeMux()
+
+	serv := &http.Server{
+		Addr:    port,
+		Handler: mux,
+	}
+
+	serv.ListenAndServe()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Adfmu1/suji-trening
+
+go 1.26.4
